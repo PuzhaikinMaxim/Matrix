91f54ba116fece13fedef4d9480c5b981c71d323
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2019-11-29 16:04:06 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Matrix a = new Matrix(1, 2, 3, 4);
         Matrix b = new Matrix(1, 2, 3, 4);
-        a = a.Matrix_multiply(a,b);
-        a.print(a);
+        Matrix c = new Matrix(4, 5, 6, 7);
+        double d;
+        /*d = a.Definer(a);
+        a.print(a.Inverse_Matrix(a));*/
+        a.print(a.Summ(a, a.Summ(a,a)));
+        /*a.print(a.Subs(a,b));
+        a.print(a.Multiply(a,8));
+        a.print(a.Matrix_multiply(a,b));*/
+        //System.out.println(d);
     }
 }
